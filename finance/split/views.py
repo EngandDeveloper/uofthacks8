67ed686c6f8d5ed68cb2ba20fb0242d5e9d10266
Jeffrey Dang <jeffrey.dang@live.com>
2021-02-20 17:30:20 -0500

    split webpage up

--- finance/split/views.py
+++ finance/split/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-urlpatterns = [
-    path('', views.expenses, name="expenses"),
-    
-]
+def split(request):
+    return render(request, 'split/index.html')
